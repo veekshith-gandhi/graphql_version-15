717c913c15d1086272c48c8448fb730925ecf26c
veekshith <veekshithgandhi@gmail.com>
2022-07-22 15:33:17 +0530

    removed console log

--- src/schema/mutation/index.js
+++ src/schema/mutation/index.js
@@ -2,15 +2,12 @@ const classResolver = require("../../../lib/classResolver");
 
 module.exports = {
   deleteEmployee(args) {
-    console.log("argss", args);
     return classResolver.deleteEmployeById(args.id);
   },
   addEmployee(args) {
-    console.log("add", args);
     return classResolver.addEmployeeDetails(args);
   },
   updateEmployee(args) {
-    console.log("update", args);
     return classResolver.updateEmployeeById(args);
   },
 };
